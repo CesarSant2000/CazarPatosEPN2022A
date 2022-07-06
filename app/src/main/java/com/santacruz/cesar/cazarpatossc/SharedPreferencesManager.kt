d98699ff34c22e798b6f256d4f5896921f1b2715
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2022-07-06 15:44:42 -0500

    upd

--- app/src/main/java/com/santacruz/cesar/cazarpatossc/SharedPreferencesManager.kt
+++ app/src/main/java/com/santacruz/cesar/cazarpatossc/SharedPreferencesManager.kt
@@ -1,13 +1,20 @@
 package com.santacruz.cesar.cazarpatossc
 
 import android.app.Activity
+import android.content.Context
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
 
 class SharedPreferencesManager (private val activity: Activity): FileHandler{
     private val masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
     override fun saveInformation(dataToStore:Pair<String,String>){
-        //val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(LOGIN_KEY , dataToStore.first)
+        editor.putString(PASSWORD_KEY, dataToStore.second)
+        editor.apply()
+    }
+    override fun saveInformationEncrypted(dataToStore:Pair<String,String>){
         val sharedPrefEncrypted = EncryptedSharedPreferences.create(
             "custom_shared_prefs_encrypted",
             masterKeyAlias,
@@ -15,14 +22,43 @@ class SharedPreferencesManager (private val activity: Activity): FileHandler{
             EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
-        //val editor = sharedPref.edit()
+        val editor = sharedPrefEncrypted.edit()
+        editor.putString(LOGIN_KEY , dataToStore.first)
+        editor.putString(PASSWORD_KEY, dataToStore.second)
+        editor.apply()
+    }
+    override fun saveInformationFile(dataToStore:Pair<String,String>){
+        val sharedPrefEncrypted = EncryptedSharedPreferences.create(
+            "custom_shared_prefs_encrypted",
+            masterKeyAlias,
+            activity,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
         val editor = sharedPrefEncrypted.edit()
         editor.putString(LOGIN_KEY , dataToStore.first)
         editor.putString(PASSWORD_KEY, dataToStore.second)
         editor.apply()
     }
     override fun readInformation():Pair<String,String>{
-        //val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+        val email = sharedPref.getString(LOGIN_KEY,"").toString()
+        val clave = sharedPref.getString(PASSWORD_KEY,"").toString()
+        return (email to clave)
+    }
+    override fun readInformationEncrypted():Pair<String,String>{
+        val sharedPrefEncrypted = EncryptedSharedPreferences.create(
+            "custom_shared_prefs_encrypted",
+            masterKeyAlias,
+            activity,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+        val email = sharedPrefEncrypted.getString(LOGIN_KEY,"").toString()
+        val clave = sharedPrefEncrypted.getString(PASSWORD_KEY,"").toString()
+        return (email to clave)
+    }
+    override fun readInformationFile():Pair<String,String>{
         val sharedPrefEncrypted = EncryptedSharedPreferences.create(
             "custom_shared_prefs_encrypted",
             masterKeyAlias,
